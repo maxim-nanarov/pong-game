7f31ab1cacb666756427d0d2ed0170af5b121104
maxim-nanarov <maxim-nanarov@cyber4s.dev>
2022-06-03 01:19:32 +0300

    added ball

--- pong-maxim-nanarov/src/app.ts
+++ pong-maxim-nanarov/src/app.ts
@@ -19,13 +19,17 @@ window.addEventListener('resize',() => {
 canvas.height = window.innerHeight -200;
 canvas.width = window.innerWidth -200;
 
-ctx.beginPath();
-ctx.setLineDash([5, 15]);
-ctx.moveTo(canvas.width/2, 0);
-ctx.fillStyle = 'White';
-ctx.lineTo(canvas.width/2, canvas.height);
 
-ctx.stroke()
+
+function drewDashLine(){
+  ctx.beginPath();
+  ctx.setLineDash([5, 15]);
+  ctx.moveTo(canvas.width/2, 0);
+  ctx.fillStyle = 'White';
+  ctx.lineTo(canvas.width/2, canvas.height);
+  ctx.stroke()
+  }
+  
 class Rectangle{
   x:number;
   y:number;
@@ -64,14 +68,14 @@ async function MOVE1(e:any) {
       if(keyPr === 87 && player1.y > 0) {
         player1.y = player1.y-10; //top arrow subtract 20 from current
       }
-      else if(keyPr === 83 && player1.y < window.innerHeight - 22){              
+      else if(keyPr === 83 && player1.y < window.innerHeight - 260){              
         player1.y = player1.y+10; //bottom arrow add 20 from current
       }
     }
   
 
   ctx.clearRect(0,0,window.innerWidth , window.innerHeight);
- 
+  drewDashLine();
   drewRect(player1.x,player1.y,player1.wid,player1.hei);
   drewRect(player2.x,player2.y,player2.wid,player2.hei);
 }
@@ -84,14 +88,14 @@ async function MOVE2(e:any) {
     if(keyPr === 38 && player2.y > 0) {
       player2.y = player2.y-10; 
     }
-    else if(keyPr === 40 && player2.y < window.innerHeight - 220){
+    else if(keyPr === 40 && player2.y < window.innerHeight - 260){
       player2.y = player2.y+10; 
     }
     
   }
 
   ctx.clearRect(0,0,window.innerWidth , window.innerHeight);
-
+  drewDashLine();
   drewRect(player1.x,player1.y,player1.wid,player1.hei);
   drewRect(player2.x,player2.y,player2.wid,player2.hei);
 }
@@ -101,48 +105,48 @@ async function MOVE2(e:any) {
   }
   
     
-// var raf:any;
+var raf:any;
 
-// var ball = {
-//   x: 100,
-//   y: 100,
-//   vx: 5,
-//   vy: 2,
-//   radius: 25,
-//   color: 'blue',
-//   draw: function() {
-//     ctx.beginPath();
-//     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-//     ctx.closePath();
-//     ctx.fillStyle = this.color;
-//     ctx.fill();
-//   }
-// };
+var ball = {
+  x: 100,
+  y: 100,
+  vx: 5,
+  vy: 2,
+  radius: 25,
+  color: 'black',
+  draw: function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+};
 
-// function draw() {
-//   ctx.clearRect(0,0, canvas.width, canvas.height);
-//   ball.draw();
-//   ball.x += ball.vx;
-//   ball.y += ball.vy;
+function draw() {
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  ball.draw();
+  ball.x += ball.vx;
+  ball.y += ball.vy;
 
-//   if (ball.y + ball.vy > canvas.height ||
-//       ball.y + ball.vy < 0) {
-//     ball.vy = -ball.vy;
-//   }
-//   if (ball.x + ball.vx > canvas.width ||
-//       ball.x + ball.vx < 0) {
-//     ball.vx = -ball.vx;
-//   }
+  if (ball.y + ball.vy > canvas.height ||
+      ball.y + ball.vy < 0) {
+    ball.vy = -ball.vy;
+  }
+  if (ball.x + ball.vx > canvas.width ||
+      ball.x + ball.vx < 0) {
+    ball.vx = -ball.vx;
+  }
 
-//   raf = window.requestAnimationFrame(draw);
-// }
+  raf = window.requestAnimationFrame(draw);
+}
 
-// canvas.addEventListener('mouseover',() => {
-//   raf = window.requestAnimationFrame(draw);
-// });
+canvas.addEventListener('mouseover',() => {
+  raf = window.requestAnimationFrame(draw);
+});
 
-// canvas.addEventListener('mouseout', () => {
-//   window.cancelAnimationFrame(raf);
-// });
+canvas.addEventListener('mouseout', () => {
+  window.cancelAnimationFrame(raf);
+});
 
-// ball.draw();
+ball.draw();
